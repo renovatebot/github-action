3ff327a8cc14ec3a618f9d7562c1c219dea61def
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-18 22:13:45 +0100

    ci: add test configuration for the e2e test and example

---
+++ test/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  platform: 'github',
+  logFileLevel: 'warn',
+  logLevel: 'debug',
+  onboarding: false,
+  dryRun: true,
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+};
