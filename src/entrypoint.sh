3e4d5f0dd6da60c4277b72bb0a26dd4dd2707dd7
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-19 20:55:40 +0100

    fix: the Dockerfile MUST have a lowercase F for GitHub Action to work

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,9 +2,20 @@
 #
 # Entrypoint for Docker.
 
-configurationFile="${1}"
+readonly CONFIGURATION_FILE="${1}"
+readonly CONFIGURATION_PATH="${GITHUB_WORKSPACE}/${CONFIGURATION_FILE}"
 
-cp "${GITHUB_WORKSPACE}/${configurationFile}" '/usr/src/app/config.js'
+if [[ ! -f "${CONFIGURATION_PATH}" ]]; then
+  echo "ERROR: Couldn't find file ${CONFIGURATION_PATH}" 1>&2
+  exit 1
+fi
+
+cp "${CONFIGURATION_PATH}" '/usr/src/app/config.js'
 
 # Run Renovate.
+#
+# This mimics the original entrypoint of the renovate/renovate Docker container.
+# See the following link for this entrypoint in the renovate/renovate Docker
+# container.
+# https://github.com/renovatebot/renovate/blob/19.175.3/Dockerfile#L220
 node /usr/src/app/dist/renovate.js
