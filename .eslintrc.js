5196b054eaf2fffee528b74cedfbb33b6f3ae586
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-23 14:07:09 +0100

    chore(lint): update ESlint config to run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'json'],
+  plugins: ['@typescript-eslint', 'prettier', 'json'],
   parserOptions: {
-    ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   env: {
-    es6: true,
+    es2020: true,
     node: true,
   },
   rules: {
-    'no-fallthrough': 'error',
-    'sort-imports': 'warn',
+    'prettier/prettier': 'error',
+    'sort-imports': 'error',
   },
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': [
-    'npm run lint-file',
-    'npm run format-file:default',
-  ],
+  '*.{ts,tsx,js,jsx,json}': ['npm run lint-file:fix'],
   '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format-file:default',
   ],
