006e61e631d54c4d3a9ef569ca9698abc3d3eea8
Michael Kriese <michael.kriese@visualon.de>
2020-04-28 06:45:00 +0200

    feat: simplify entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,20 +5,12 @@
 readonly CONFIGURATION_FILE="${1}"
 readonly TOKEN="${2}"
 
-readonly CONFIGURATION_PATH="${GITHUB_WORKSPACE}/${CONFIGURATION_FILE}"
+export RENOVATE_CONFIG_FILE="${GITHUB_WORKSPACE}/${CONFIGURATION_FILE}"
 
-if [[ ! -f "${CONFIGURATION_PATH}" ]]; then
-  echo "ERROR: Couldn't find file ${CONFIGURATION_PATH}" 1>&2
+if [[ ! -f "${RENOVATE_CONFIG_FILE}" ]]; then
+  echo "ERROR: Couldn't find file ${RENOVATE_CONFIG_FILE}" 1>&2
   exit 1
 fi
-
-readonly WORKDIR='/usr/src/app'
-# Account for the WORKDIR entry of the renovate/renovate Docker container. See
-# the followling link for this entry.
-# https://github.com/renovatebot/renovate/blob/19.175.3/Dockerfile#L14
-cd "${WORKDIR}" || { echo "ERROR: Couldn't cd to ${WORKDIR}" 1>&2; exit 1; }
-
-cp "${CONFIGURATION_PATH}" './config.js'
 
 # Run Renovate.
 #
