ed1f35642e98e568dcd4b22d37100a3768b2e7a2
Michael Kriese <michael.kriese@visualon.de>
2020-04-29 07:18:46 +0200

    chore: pass to default entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,10 +2,8 @@
 #
 # Entrypoint for Docker.
 
-readonly CONFIGURATION_FILE="${1}"
-readonly TOKEN="${2}"
-
-export RENOVATE_CONFIG_FILE="${GITHUB_WORKSPACE}/${CONFIGURATION_FILE}"
+export RENOVATE_TOKEN="${2}"
+export RENOVATE_CONFIG_FILE="${GITHUB_WORKSPACE}/${1}"
 
 if [[ ! -f "${RENOVATE_CONFIG_FILE}" ]]; then
   echo "ERROR: Couldn't find file ${RENOVATE_CONFIG_FILE}" 1>&2
@@ -17,4 +15,5 @@ fi
 # Mimic the original ENTRYPOINT of the renovate/renovate Docker container. See
 # the following link for this entry.
 # https://github.com/renovatebot/docker-renovate/blob/d3aa0d99931ea7ad7e901a1e538eba0d61268229/Dockerfile#L63
-RENOVATE_TOKEN="${TOKEN}" renovate
+
+/usr/local/bin/docker-entrypoint.sh
