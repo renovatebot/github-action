4fa9cad2cc1fb77e5bdb2664cb180bcc05f954f8
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-05-03 19:14:08 +0200

    fix: mount volume with config file to Docker container

--- src/renovate.ts
+++ src/renovate.ts
@@ -1,26 +1,52 @@
 import Docker from './docker';
 import child from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 class Renovate {
-  private configurationFileEnv = 'RENOVATE_CONFIG_FILE';
+  private configFileEnv = 'RENOVATE_CONFIG_FILE';
   private tokenEnv = 'RENOVATE_TOKEN';
+  private configFileMountDir = '/github-action';
 
+  private configFile: string;
   private docker: Docker;
 
-  constructor(private configurationFile: string, private token: string) {
+  constructor(configFile: string, private token: string) {
+    this.configFile = path.resolve(configFile);
+
+    this.validateArguments();
+
     this.docker = new Docker();
   }
 
   runDockerContainer(): void {
     const commandArguments = [
       '--rm',
-      `--env ${this.configurationFileEnv}='${this.configurationFile}'`,
+      `--env ${this.configFileEnv}='${this.configFileMountPath()}'`,
       `--env ${this.tokenEnv}='${this.token}'`,
+      `--volume ${this.configFile}:${this.configFileMountPath()}`,
       this.docker.image(),
     ];
     const command = `docker run ${commandArguments.join(' ')}`;
+    // console.log(commandArguments, command);
 
     child.execSync(command, { stdio: 'inherit' });
+  }
+
+  private validateArguments(): void {
+    if (!fs.existsSync(this.configFile)) {
+      throw new Error(
+        `Could not locate configuration file '${this.configFile}'.`,
+      );
+    }
+  }
+
+  private configFileName(): string {
+    return path.basename(this.configFile);
+  }
+
+  private configFileMountPath(): string {
+    return path.join(this.configFileMountDir, this.configFileName());
   }
 }
 
