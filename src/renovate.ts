a5677bef1752e1d3187879c652b100236e6bb33e
Michael Kriese <michael.kriese@visualon.de>
2021-08-12 22:37:49 +0200

    fix: do not pass docker user

--- src/renovate.ts
+++ src/renovate.ts
@@ -36,7 +36,7 @@ class Renovate {
     dockerArguments.push(
       '--volume /var/run/docker.sock:/var/run/docker.sock',
       '--volume /tmp:/tmp',
-      `--user ${renovateDockerUser}:${this.getDockerGroupId()}`,
+      // `--user ${renovateDockerUser}:${this.getDockerGroupId()}`,
       '--rm',
       this.docker.image()
     );
