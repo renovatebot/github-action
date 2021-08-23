64675913567b97f845892a70e32518d33b551aed
Michael Kriese <michael.kriese@visualon.de>
2021-08-23 12:26:34 +0200

    chore: code cleanup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,4 @@
 module.exports = {
   '*.{ts,tsx,js,jsx,json}': ['yarn lint-es:file:fix'],
-  '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
-    'yarn format:file',
-  ],
+  '*.{js,jsx,ts,tsx,md,yml,yaml,json}': ['yarn format:file'],
 };
