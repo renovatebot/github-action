828b013112d1b65b1ee63438e0e271765b0ad64a
Michael Kriese <michael.kriese@visualon.de>
2021-08-23 11:51:17 +0200

    build: use yarn (#566)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': ['npm run lint-es:file:fix'],
+  '*.{ts,tsx,js,jsx,json}': ['yarn lint-es:file:fix'],
   '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
-    'npm run format:file',
+    'yarn format:file',
   ],
 };
