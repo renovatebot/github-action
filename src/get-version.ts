2333429cf09136ec9a93fa1abc7fa128a20ee6f8
Michael Kriese <michael.kriese@visualon.de>
2023-02-03 11:15:51 +0100

    fix: broken release

--- src/get-version.ts
+++ src/get-version.ts
@@ -1,7 +1,4 @@
 import * as core from '@actions/core';
 import Docker from './docker';
-import { Input } from './input';
 
-const input = new Input();
-const docker = new Docker(input);
-core.setOutput('version', docker.version());
+core.setOutput('version', Docker.version());
