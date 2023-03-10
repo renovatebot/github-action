60c620e2e419494dc033408808b7faf2e266ab01
Michael Kriese <michael.kriese@visualon.de>
2023-03-10 15:19:16 +0100

    feat!: use semantic release
    BREAKING CHANGE: Action does no longer follow Renovate versions.
     Use `renovate-version` option to pin to a specific version.

--- src/docker.ts
+++ src/docker.ts
@@ -1,23 +1,20 @@
 import type { Input } from './input';
 
-// renovate: datasource=docker depName=renovate/renovate versioning=docker
-const tag = '35.2.0-slim';
-
 class Docker {
   private static readonly repository = 'renovate/renovate';
-  private static readonly tagSuffix = '-slim';
   private readonly fullTag: string;
 
   constructor(input: Input) {
-    this.fullTag = input.useSlim() ? tag : tag.replace(Docker.tagSuffix, '');
+    const tag = input.getVersion();
+    this.fullTag = input.useSlim()
+      ? tag
+        ? `${tag}-slim`
+        : 'slim'
+      : tag ?? 'latest';
   }
 
   image(): string {
     return `${Docker.repository}:${this.fullTag}`;
-  }
-
-  static version(): string {
-    return tag.replace(Docker.tagSuffix, '');
   }
 }
 
