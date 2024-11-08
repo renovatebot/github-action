7aa0719b3d7ab09d2f287ff9e9b77419d70ca853
Michael Kriese <michael.kriese@visualon.de>
2024-11-08 16:10:51 +0100

    build: use `esbuild`

---
+++ tools/compile.js
@@ -0,0 +1,14 @@
+import { build } from 'esbuild';
+import { env } from 'node:process';
+
+await build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  target: 'node20',
+  minify: !!env['CI'],
+  tsconfig: 'tsconfig.dist.json',
+  sourcemap: true,
+  // format: 'esm', // TODO: later PR
+  outdir: './dist/',
+});
