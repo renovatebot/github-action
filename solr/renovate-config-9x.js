1623e63c88a3a3cf7be58ef79910cdb27775585f
Jan Høydahl <janhoy@users.noreply.github.com>
2025-01-23 01:33:02 +0100

    Prepare for separate branch_9x runs

---
+++ solr/renovate-config-9x.js
@@ -0,0 +1,12 @@
+module.exports = {
+  gitAuthor: 'SolrBot <solrbot@cominvent.com>',
+  onboarding: false,
+  requireConfig: "required",
+  platform: 'github',
+  forkProcessing: "disabled",
+  repositories: ['apache/solr'],
+  baseBranches: ["branch_9x"],
+  allowedPostUpgradeCommands: ["./gradlew.*"],
+  branchPrefix: "renovate-9x/",
+  dryRun: "extract",
+};
