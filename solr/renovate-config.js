626c1a372a595af2e871accfa47a67ee17feae16
Jan Høydahl <janhoy@users.noreply.github.com>
2025-01-23 14:02:31 +0100

    Use baseBranches config on main runs

--- solr/renovate-config.js
+++ solr/renovate-config.js
@@ -7,5 +7,8 @@ module.exports = {
   repositories: [
     'apache/solr'
   ],
+  // Do independent upgrades for main and branch_9x. See renovate.json overlay file on branch_9x
+  baseBranches: ["main", "branch_9x"],
+  useBaseBranchConfig: "merge",
   allowedPostUpgradeCommands: ["./gradlew.*"]
 };
